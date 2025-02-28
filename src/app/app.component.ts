e136999f419caacd0793e12755fff437d3d04d11
zaiets.sv <zaiets.sv10.dev@gmail.com>
2025-02-28 16:53:01 +0200

    feat: setup navigation with PrimeNG Menubar and added search-form and timezone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import {Menubar} from "primeng/menubar";
+import {MenuItem} from "primeng/api";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menubar, RouterLink, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-test-tasks';
+export class AppComponent implements OnInit {
+  public title = 'angular-test-tasks';
+  public items: MenuItem[] | undefined;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        route: '/'
+      },
+      {
+        label: 'Tasks',
+        icon: 'pi pi-th-large',
+        items: [
+          {
+            label: 'Timezone',
+            icon: 'pi pi-calendar',
+            route: '/timezone'
+          },
+          {
+            icon: 'pi pi-search',
+            label: 'Search Form',
+            route: '/search-form'
+          }
+        ]
+      },
+      {
+        label: 'External',
+        icon: 'pi pi-server',
+        items: [
+          {
+            label: 'Angular',
+            url: 'https://angular.io/'
+          },
+          {
+            label: 'Vite.js',
+            url: 'https://vitejs.dev/'
+          }
+        ]
+      }
+    ];
+  }
 }
---
+++ src/app/components/search-form/search-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-form',
+  imports: [],
+  templateUrl: './search-form.component.html',
+  styleUrl: './search-form.component.scss'
+})
+export class SearchFormComponent {
+
+}
